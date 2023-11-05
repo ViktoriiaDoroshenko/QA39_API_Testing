2a487bcbc6b47e6f76e8b18d3a6d82529c6e7968
ViktoriiaDoroshenko <Emi90va@gmail.com>
2023-11-05 13:46:09 +0200

    HW21

--- src/test/java/okhttp/RegistrationTests.java
+++ src/test/java/okhttp/RegistrationTests.java
@@ -1,5 +1,51 @@
 package okhttp;
 
-public class RegistrationTests {
+import com.google.gson.Gson;
+import dto.AuthRequestDTO;
+import dto.AuthResponseDTO;
+import dto.ErrorDTO;
+import okhttp3.*;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
+import java.io.IOException;
+
+public class RegistrationTests {
+    // token = eyJhbGciOiJIUzI1NiJ9.eyJyb2xlcyI6WyJST0xFX1VTRVIiXSwic3ViIjoia2lyYTgwQGdtYWlsLmNvbSIsImlzcyI6IlJlZ3VsYWl0IiwiZXhwIjoxNjk5Nzg0NzA3LCJpYXQiOjE2OTkxODQ3MDd9.Wd4ZB1mUS6OLGdK4tCwP1d6h0TYxt4i1gOGztnN1ZhY
+    public static final MediaType JSON = MediaType.get("application/json;charset=utf-8");
+    Gson gson = new Gson();
+    OkHttpClient client = new OkHttpClient();
+    String baseURL = "https://contactapp-telran-backend.herokuapp.com";
+
+    @Test
+    public void loginPositive() throws IOException {
+
+        AuthRequestDTO requestDTO = AuthRequestDTO.builder()
+                .username("kira80@gmail.com")
+                .password("2080Kira!")
+                .build();
+        RequestBody requestBody = RequestBody.create(gson.toJson(requestDTO),JSON);
+
+        Request request = new Request.Builder()
+                .url(baseURL+"/v1/user/registration/usernamepassword")
+                .post(requestBody)
+                .build();
+
+        Response response = client.newCall(request).execute();
+
+        if(response.isSuccessful()){
+            String responseJson = response.body().string();
+            AuthResponseDTO responseDTO = gson.fromJson(responseJson, AuthResponseDTO.class);
+            System.out.println("Response code is ---> " + response.code());
+            System.out.println(responseDTO.getToken());
+            Assert.assertTrue(response.isSuccessful());
+        }else {
+            ErrorDTO errorDTO = gson.fromJson(response.body().string(), ErrorDTO.class);
+            System.out.println("Response code is ---> " + response.code());
+            System.out.println(errorDTO.getStatus()+" ==== "
+                    + errorDTO.getMessage()+" ==== " + errorDTO.getError());
+            Assert.assertFalse(response.isSuccessful());
+        }
+
+    }
 }
